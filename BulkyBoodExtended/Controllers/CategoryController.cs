3758d346b584e54b3cd215c59930d5b639e4ef8e
timiking5 <tima310103@gmail.com>
2023-05-17 21:06:33 +0300

    First steps

---
+++ BulkyBoodExtended/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBoodExtended.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBoodExtended.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext db)
+        {
+
+            _dbContext = db;
+
+        }
+        public IActionResult Index()
+        {
+            var categoryList = _dbContext.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
