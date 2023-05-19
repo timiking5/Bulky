ba1f5800ad59ff7efbfc0b72c91a3a124c4b0be4
timiking5 <tima310103@gmail.com>
2023-05-19 17:50:22 +0300

    Repository

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    internal interface IRepository
+    public interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T GetById(Expression<Func<T, bool>> filter);
+        void Create(T entity);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
     }
 }
